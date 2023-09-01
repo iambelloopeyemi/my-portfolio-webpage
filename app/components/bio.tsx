fda0f4fa9f0ee38f227184d29ae8bd11c8f14eb5
Opeyemi Bello <opeyemiemmanuelbello@gmail.com>
2023-09-01 16:06:38 +0100

    update my bio

--- app/components/bio.tsx
+++ app/components/bio.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 export default function Bio() {
   return (
     <article className="text-dim-gray text-justify leading-6 grid gap-2">
@@ -7,26 +5,20 @@ export default function Bio() {
         I&apos;m a Frontend Web Developer committed to crafting functional and
         interactive web applications that offer a world-class user experience.
         Check out some of my work in the&nbsp;
-        <Link href={`#projects`} className="text-violet-blue underline">
-          <strong>Projects</strong>
-        </Link>
+        <strong className="text-violet-blue underline">Projects</strong>
         &nbsp;section.
       </p>
       <p>
         I also enjoy writing articles and sharing content related to what
         I&apos;ve learned, aiming to assist others within the Dev Community.
         Feel free to explore some of my work in the&nbsp;
-        <Link href={`#articles`} className="text-violet-blue underline">
-          <strong>Articles</strong>
-        </Link>
+        <strong className="text-violet-blue">Articles</strong>
         &nbsp;section.
       </p>
       <p>
         If you&apos;re interested in exploring potential opportunities or
         discussing collaboration, please don&apos;t hesitate to&nbsp;
-        <Link href={`#contact`} className="text-violet-blue underline">
-          <strong>Contact</strong>
-        </Link>
+        <strong className="text-violet-blue">Contact</strong>
         &nbsp;me. I&apos;m looking forward to connecting with you!
       </p>
     </article>
