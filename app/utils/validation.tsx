ab06ab03aa3eafa7ad74add0e7f5dcb680f44676
Opeyemi Bello <opeyemiemmanuelbello@gmail.com>
2023-09-07 14:47:12 +0100

    add phone number field to the contact form

--- app/utils/validation.tsx
+++ app/utils/validation.tsx
@@ -3,5 +3,6 @@ import * as Yup from "yup";
 export const validationSchema = Yup.object().shape({
   name: Yup.string().required("Your Full Name is required"),
   email: Yup.string().email("Invalid email").required("Your Email is required"),
+  phone: Yup.string().required("Your Phone Number is required"),
   message: Yup.string().required("Your Message is required"),
 });
